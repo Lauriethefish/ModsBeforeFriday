00c0373f770ae09c3b401f1d5c55a854a302448d
Lauriethefish <lauriethefish@outlook.com>
2024-04-04 19:57:11 +0100

    Add build script to download libsl2.so/libmain.so

---
+++ mbf-agent/build.rs
@@ -0,0 +1,27 @@
+use std::{fs::OpenOptions, path::Path};
+
+const LIBMAIN_URL: &str = "https://github.com/sc2ad/LibMainLoader/releases/download/v0.1.0-alpha/libmain.so";
+const SL2_URL: &str = "https://github.com/sc2ad/Scotland2/releases/latest/download/libsl2.so";
+
+fn download_if_not_exist(url: &str, to: &str) {
+    if Path::new(to).exists() {
+        return;
+    }
+
+    println!("Downloading {url} to {to:?}");
+    let mut reader = ureq::get(url).call().expect("Failed to access URL")
+        .into_reader();
+    let mut writer = OpenOptions::new()
+        .create_new(true)
+        .write(true)
+        .open(to)
+        .expect("Failed to create output file");
+
+    std::io::copy(&mut reader, &mut writer).expect("Failed to download file");
+}
+
+fn main() {
+    std::fs::create_dir_all("./libs").expect("Failed to create ./libs");
+    download_if_not_exist(LIBMAIN_URL, "./libs/libmain.so");
+    download_if_not_exist(SL2_URL, "./libs/libsl2.so");
+}
