c4c5311620170acf503a1fc85c3b5d67ade9dc82
Lauriethefish <lauriethefish@outlook.com>
2024-04-05 12:48:15 +0100

    Enable HTTPS for development

--- mbf-site/vite.config.ts
+++ mbf-site/vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
+import mkcert from 'vite-plugin-mkcert'
 
 export default defineConfig({
     base: process.env.BASE_URL ?? '/',
-    plugins: [react(), viteTsconfigPaths()],
+    plugins: [react(), viteTsconfigPaths(), mkcert()],
     server: {    
         open: true,
         port: 3000,
-    }
+        https: true
+    },
 })
