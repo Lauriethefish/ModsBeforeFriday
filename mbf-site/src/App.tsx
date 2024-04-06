e7b7ac8e1fd2053fbad29b5c25655040b04c6e53
Lauriethefish <lauriethefish@outlook.com>
2024-04-06 06:56:05 +0100

    Add instructions for inverting connection and warnings about iOS

--- mbf-site/src/App.tsx
+++ mbf-site/src/App.tsx
@@ -64,13 +64,7 @@ function ChooseDevice() {
           <Title />
           <p>To get started, plug your Quest in with a USB-C cable and click the button below.</p>
 
-          <h3>No compatible devices?</h3>
-
-          <p>
-            To use MBF, you must enable developer mode so that your Quest is accessible via USB.
-            <br />Follow the <a href="https://developer.oculus.com/documentation/native/android/mobile-device-setup/?locale=en_GB">official guide</a> -
-            you'll need to create a new organisation and enable USB debugging.
-          </p>
+          <NoCompatibleDevices />
 
           <button id="chooseDevice" onClick={async () => {
             let device: Adb | null;
@@ -138,10 +132,39 @@ function isViewingOnMobile() {
   return (navigator.maxTouchPoints ?? 0) > 0;
 }
 
+// Kindly provided by Pierre
+// https://stackoverflow.com/a/9039885
+function isViewingOnIos() {
+  return [
+    'iPad Simulator',
+    'iPhone Simulator',
+    'iPod Simulator',
+    'iPad',
+    'iPhone',
+    'iPod'
+    // This is deprecated but still provides a good way to detect iOS as far as the author is concerned.
+    // We are also doing feature detection for WebUSB, but detecting iOS provides a good way to warn the user that no iOS browsers will work with this app.
+  ].includes(navigator.platform)
+  // iPad on iOS 13 detection
+  || (navigator.userAgent.includes("Mac") && "ontouchend" in document)
+}
+
 function UnsupportedMessage() {
-  return <div className='container'>
-    <h1>Browser Unsupported</h1>
-    <p>It looks like your browser doesn't support WebUSB, which this app needs to be able to access your Quest's files.</p>
+  return <div className='container mainContainer'>
+    {isViewingOnIos() ? <>
+      <h1>iOS is not supported</h1>
+      <p>MBF has detected that you're trying to use it from an iOS device. Unfortunately, Apple does not allow WebUSB, which MBF needs to be able to interact with the Quest.</p>
+      <p>To mod your game, you will need one of: </p>
+      <ul>
+        <li>A PC or Mac (preferred)</li>
+        <li>An Android phone (still totally works)</li>
+      </ul>
+
+      <p>.... and one of the following supported browsers:</p>
+    </> : <>
+      <h1>Browser Unsupported</h1>
+      <p>It looks like your browser doesn't support WebUSB, which this app needs to be able to access your Quest's files.</p>
+    </>}
 
     <h2>Supported Browsers</h2>
     <SupportedBrowsers />
@@ -164,10 +187,34 @@ function SupportedBrowsers() {
         <li>Opera 48 or newer</li>
         <li>Microsoft Edge 79 or newer</li>
       </ul>
-      <h3 className='fireFox'>Firefox is NOT supported.</h3>
+      <h3 className='fireFox'>Firefox and Safari are NOT supported.</h3>
       <p>(There is no feasible way to add support for Firefox as Mozilla have chosen not to support WebUSB for security reasons.)</p>
     </>
   }
+}
+
+function NoCompatibleDevices() {
+  return <>
+    <h3>No compatible devices?</h3>
+
+    <p>
+      To use MBF, you must enable developer mode so that your Quest is accessible via USB.
+      <br />Follow the <a href="https://developer.oculus.com/documentation/native/android/mobile-device-setup/?locale=en_GB" target="_blank" rel="noopener noreferrer">official guide</a> -
+      you'll need to create a new organisation and enable USB debugging.
+    </p>
+
+    {isViewingOnMobile() && <>
+      <h4>Using Android?</h4>
+      <p>It's possible that the connection between your device and the Quest has been set up the wrong way around. To fix this:</p>
+      <ul>
+        <li>Swipe down from the top of the screen.</li>
+        <li>Click the dialog relating to the USB connection. This might be called "charging via USB".</li>
+        <li>Change "USB controlled by" to "Connected device". If "Connected device" is already selected, change it to "This device" and change it back.</li>
+      </ul>
+      <h4>Still not working?</h4>
+      <p>Try unplugging your cable and plugging the end that's currently in your phone into your Quest.</p>
+    </>}
+  </>
 }
 
 export default App;
