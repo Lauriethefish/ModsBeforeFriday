e8ccd7156765754bc3a62dc15b5de97cd036caf8
Lauriethefish <lauriethefish@outlook.com>
2024-04-11 19:09:25 +0100

    Hide collapsible by default

--- mbf-site/src/components/Collapsible.tsx
+++ mbf-site/src/components/Collapsible.tsx
@@ -4,7 +4,7 @@ import '../css/Collapsible.css';
 
 // A menu that can be collapsed and expanded with a mouse click.
 export function Collapsible({ title, children }: { title: string, children: ReactNode }) {
-    const [expanded, setExpanded] = useState(true);
+    const [expanded, setExpanded] = useState(false);
 
     return <div className="collapsible">
         <div className="collapsibleTitle" onClick={() => setExpanded(!expanded)}>
