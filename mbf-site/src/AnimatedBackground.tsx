a13927e2c8db7d44dea79b926c1324cfd2b400f7
Lauriethefish <lauriethefish@outlook.com>
2024-04-12 09:00:09 +0100

    Remove unneeded import

--- mbf-site/src/AnimatedBackground.tsx
+++ mbf-site/src/AnimatedBackground.tsx
@@ -1,5 +1,4 @@
 import "./css/AnimatedBackground.css";
-import { useCallback, useEffect, useRef, useState } from 'react';
 
 //	Speed (in pixels per millisecond) at which the blocks will move
 const BLOCK_FALL_SPEED = 10 /1000;
