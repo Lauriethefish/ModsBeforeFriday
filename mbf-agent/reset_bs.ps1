d9f8527af9d2f6cc0258a566d66017a9f8ae0034
Lauriethefish <lauriethefish@outlook.com>
2024-04-27 21:49:50 +0100

    Update shell scripts to put APKS in subdirectory

--- mbf-agent/reset_bs.ps1
+++ mbf-agent/reset_bs.ps1
@@ -1,13 +1,14 @@
 $Latest=$args[0]
+$APKS_PATH = "./apk_data"
 
 # Put your OBB file/BS APK here
 if ( $Latest.Length -ne 0 ) {
     Write-Output "Using latest: 1.36.1"
-    $OBB_PATH = "main.1194.com.beatgames.beatsaber.obb"
-    $BS_PATH = "bs1361.apk"
+    $OBB_PATH = "$APKS_PATH/main.1194.com.beatgames.beatsaber.obb"
+    $BS_PATH = "$APKS_PATH/bs1361.apk"
 }   else    {
-    $OBB_PATH = "main.1130.com.beatgames.beatsaber.obb"
-    $BS_PATH = "bs135.apk"
+    $OBB_PATH = "$APKS_PATH/main.1130.com.beatgames.beatsaber.obb"
+    $BS_PATH = "$APKS_PATH/bs135.apk"
 }
 
 Write-Output "Resetting to vanilla BS"
