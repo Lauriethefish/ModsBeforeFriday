b71d34c61bd0ca4d0950cc0ec1c54841848c395f
Lauriethefish <lauriethefish@outlook.com>
2024-06-25 08:41:05 +0100

    Fix build script/fix incorrect OBB path

--- mbf-agent/reset_bs.ps1
+++ mbf-agent/reset_bs.ps1
@@ -4,15 +4,17 @@ $APKS_PATH = "./apk_data"
 # Put your OBB file/BS APK here
 if ( $Latest.Length -ne 0 ) {
     Write-Output "Using latest: 1.36.1"
-    $OBB_PATH = "$APKS_PATH/main.1194.com.beatgames.beatsaber.obb"
+	$OBB_NAME = "main.1194.com.beatgames.beatsaber.obb"
     $BS_PATH = "$APKS_PATH/bs1361.apk"
 }   else    {
-    $OBB_PATH = "$APKS_PATH/main.1130.com.beatgames.beatsaber.obb"
+    $OBB_NAME = "main.1130.com.beatgames.beatsaber.obb"
     $BS_PATH = "$APKS_PATH/bs135.apk"
 }
+
+$OBB_PATH = "$APKS_PATH/$OBB_NAME"
 
 Write-Output "Resetting to vanilla BS"
 adb shell pm uninstall com.beatgames.beatsaber
 adb install $BS_PATH
-adb push $OBB_PATH "/sdcard/Android/obb/com.beatgames.beatsaber/$OBB_PATH"
+adb push $OBB_PATH "/sdcard/Android/obb/com.beatgames.beatsaber/$OBB_NAME"
 adb kill-server
