e10234f5f616b03b3af3f1bebb3e4bc8f34b0424
Lauriethefish <lauriethefish@outlook.com>
2024-07-13 18:35:41 +0100

    Further improvements to performance of color scheme fixes Reading to string is faster than BufReader.

--- mbf-agent/src/data_fix.rs
+++ mbf-agent/src/data_fix.rs
@@ -1,12 +1,14 @@
-use std::{fs::OpenOptions, io::{BufReader, BufWriter}, path::Path};
+use std::{fs::{File, OpenOptions}, io::{Read, Write}, path::Path};
 use anyhow::{Context, Result, anyhow};
 
 // Fixes issues with player colour schemes from 1.28 loading incorrectly on v1.35.0 or newer.
 pub fn fix_colour_schemes(path: impl AsRef<Path>) -> Result<()> {
-    let mut player_data: serde_json::Value = {
-        let mut reader = BufReader::new(std::fs::File::open(&path)?);
-        serde_json::from_reader(&mut reader).context("Player data was invalid JSON")?
-    };
+    let mut data_file_buf = Vec::with_capacity(8192);
+    File::open(&path)
+        .context("Failed to open player data file for reading")?
+        .read_to_end(&mut data_file_buf)?;
+
+    let mut player_data: serde_json::Value = serde_json::from_slice(&data_file_buf).context("Player data was invalid JSON")?;
 
     let local_players = player_data.get_mut("localPlayers")
         .ok_or(anyhow!("No localPlayers array found"))?
@@ -22,11 +24,15 @@ pub fn fix_colour_schemes(path: impl AsRef<Path>) -> Result<()> {
         color_schemes_settings.insert("selectedColorSchemeId".to_string(), "User0".into());
     }
 
-    let mut writer = BufWriter::new(OpenOptions::new()
+
+    let output_str = serde_json::to_string(&player_data)
+        .context("Failed to write player data")?;
+
+    let mut writer = OpenOptions::new()
         .write(true)
         .truncate(true)
-        .open(path).context("Failed to open player data file for writing")?);
+        .open(&path).context("Failed to open player data file for writing")?;
+    writer.write_all(output_str.as_bytes())?;
 
-    serde_json::to_writer(&mut writer, &player_data).context("Failed to write player data")?;
     Ok(())
 }
