036e157b279d4abb1b062e7a91852c6f4b79c8cb
Lauriethefish <lauriethefish@outlook.com>
2024-08-18 11:18:42 +0100

    Add write timeout to ureq agent

--- mbf-res-man/src/external_res.rs
+++ mbf-res-man/src/external_res.rs
@@ -30,6 +30,8 @@ const CORE_MODS_URL: &str = "https://raw.githubusercontent.com/QuestPackageManag
 
 // If no data is read for this period of time during a file download, the download will be failed.
 pub const REQUEST_TIMEOUT_READ_SECS: u64 = 20;
+// If no data is written for this period of time during a file download, the download will be failed
+pub const REQUEST_TIMEOUT_WRITE_SECS: u64 = 20;
 
 static AGENT: sync::OnceLock<ureq::Agent> = sync::OnceLock::new();
 
@@ -37,6 +39,7 @@ pub fn get_agent() -> &'static ureq::Agent {
     AGENT.get_or_init(|| {
         ureq::AgentBuilder::new()
             .timeout_read(Duration::from_secs(REQUEST_TIMEOUT_READ_SECS))
+            .timeout_write(Duration::from_secs(REQUEST_TIMEOUT_WRITE_SECS))
             .https_only(true)
             .try_proxy_from_env(true)
             .user_agent(format!("mbf-agent/{}", env!("CARGO_PKG_VERSION")).as_str())
