50c1be0e30ec1e11f9d1d7419a831246ac76f164
Lauriethefish <lauriethefish@outlook.com>
2024-08-21 21:56:50 +0100

    Add different graphics for each log level Warning/Error: orange/red transparent bg, warning triangle/error circle Debug: reduced opacity. TODO: Disable debug logs by default and add option to re-enable.

--- mbf-site/src/components/LogWindow.tsx
+++ mbf-site/src/components/LogWindow.tsx
@@ -1,9 +1,30 @@
-import { useContext, useEffect } from 'react';
+import { useEffect } from 'react';
 import '../css/LogWindow.css';
-import { LogMsg } from '../Messages';
 import '../fonts/Consolas.ttf';
+import AlertTriangle from '../icons/alert-triangle.svg';
+import AlertCircle from '../icons/alert-circle.svg';
 import { useLogStore } from '../Logging';
+import { LogMsg } from '../Messages';
 
+export function LogItem({ event }: { event: LogMsg }) {
+    switch(event.level) {
+        case 'Warn':
+            return <span className="logItem logWithIcon logWarning">
+                <img src={AlertTriangle} alt="A warning triangle" width="20" />
+                {event.message}
+            </span>
+        case 'Error':
+            return <span className="logItem logWithIcon logError">
+                <img src={AlertCircle} alt="An error icon" width="20" />
+                {event.message}
+            </span>
+        case 'Debug':
+        case 'Trace':
+            return <p className="logItem logDebug">{event.message}</p>
+        default:
+            return <p className="logItem">{event.message}</p>
+    }
+}
 
 export function LogWindow() {
     const { logEvents } = useLogStore();
@@ -15,7 +36,7 @@ export function LogWindow() {
     })
 
     return <div id="logWindow" className="codeBox">
-        {logEvents.map((event, idx) => <p className="logItem" key={idx}>{event.message}</p>)}
+        {logEvents.map((event, idx) => <LogItem event={event} key={idx} />)}
 
         <div ref={element => { bottomDiv = element }}/>
     </div>
