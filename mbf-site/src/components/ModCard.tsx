0f671ffa05d000a653dee7d9325cc9d2018b9ad0
Lauriethefish <lauriethefish@outlook.com>
2024-08-21 10:27:34 +0100

    Add warnings for disabling or removing core mods

--- mbf-site/src/components/ModCard.tsx
+++ mbf-site/src/components/ModCard.tsx
@@ -20,8 +20,22 @@ function CoreModBadge() {
     </div>
 }
 
+function CoreModWarning(props: { mod: Mod }) {
+    return <>
+        <p className="warning">{props.mod.name} is a <em>core mod</em>.</p>
+        <p>
+            These mods are automatically installed and managed by MBF. Removing or disabling them will most likely <b>prevent
+            your custom songs from loading</b> or have other <b>unforseen consequences.</b>
+        </p>
+        <p className="warning">
+            You have been warned.
+        </p>
+    </>;
+}
+
 export function ModCard(props: ModCardProps) {
     const [requestRemove, setRequestRemove] = useState(false);
+    const [requestDisable, setRequestDisable] = useState(false);
     const [wrongGameVersion, setWrongGameVersion] = useState(false);
     const setEnabled = (enabled: boolean) => {
         props.onEnabledChanged(enabled);
@@ -45,30 +59,51 @@ export function ModCard(props: ModCardProps) {
             </div>
             <Slider on={props.mod.is_enabled} valueChanged={value => {
                 if(value && props.mod.game_version != null 
-                    && props.mod.game_version !== props.gameVersion) {
+                    && props.mod.game_version !== props.gameVersion
+                    && !props.mod.is_core) { // Do not show the wrong game version prompt for core mods.
+                        // This is because modders sometimes forget to update the game version, but if the mod is core
+                        // then we know it's designed for the current version anyway, so there's no need for the prompt.
                     setWrongGameVersion(true);
                 }   else    {
-                    setEnabled(value);
+                    if(!value && props.mod.is_core) {
+                        setRequestDisable(true);
+                    }   else    {
+                        setEnabled(value);
+                    }
                 }
             }}/>
         </div>
 
         <YesNoModal
-            title="Confirm"
+            title={props.mod.is_core ? "Remove core mod " : "Confirm removal"}
             onYes={() => {
                 setRequestRemove(false);
                 props.onRemoved();
             }}
             onNo={()=> setRequestRemove(false)}
             isVisible={requestRemove}>
-            <p>Are you sure that you want to remove {props.mod.id} v{props.mod.version}?</p>
+            {props.mod.is_core && <CoreModWarning mod={props.mod} />}
+
+            <p>Are you sure that you want to remove {props.mod.name} v{props.mod.version}?</p>
         </YesNoModal>
+        <YesNoModal title="Disable core mod"
+            onYes={() => {
+                setRequestDisable(false);
+                setEnabled(false);
+            }}
+            onNo={() => setRequestDisable(false)}
+            isVisible={requestDisable}>
+            <CoreModWarning mod={props.mod} />
+
+            <p>Are you still sure that you want to disable {props.mod.name} v{props.mod.version}?</p>
+        </YesNoModal>
+
         <YesNoModal title="Wrong game version"
             onYes={() => { setEnabled(true); setWrongGameVersion(false) }}
             onNo={() => setWrongGameVersion(false)} 
             isVisible={wrongGameVersion}>
-            <p>The mod {props.mod.id} v{props.mod.version} is designed for game version {props.mod.game_version === null ? null : trimGameVersion(props.mod.game_version)} but you have {trimGameVersion(props.gameVersion)}.</p>
-            <p className="warning">It is EXTREMELY likely that enabling it will crash your game and/or mess up your mods in a way that could be VERY DIFFICULT to undo.</p>
+            <p>The mod {props.mod.name} v{props.mod.version} is designed for game version {props.mod.game_version === null ? null : trimGameVersion(props.mod.game_version)} but you have {trimGameVersion(props.gameVersion)}.</p>
+            <p className="warning">It is EXTREMELY likely that enabling it will crash your game and mess up your mods in a way that could be VERY DIFFICULT to undo.</p>
             <p>Are you sure you still want to enable it (you don't)?</p>
         </YesNoModal>
     </div>
