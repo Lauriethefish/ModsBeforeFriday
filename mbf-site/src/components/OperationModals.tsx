793ec286858a4bd6032185cefd0e9b873869e25e
Daniel <790119+DanTheMan827@users.noreply.github.com>
2025-10-03 19:21:25 -0500

    Refactor OperationModals to not use Valtio

--- mbf-site/src/components/OperationModals.tsx
+++ mbf-site/src/components/OperationModals.tsx
@@ -9,10 +9,8 @@
 import { ScaleLoader } from "react-spinners";
 import { LogWindow, LogWindowControls } from "./LogWindow";
 import { ErrorModal } from "./Modal";
-import React from "react";
-import { proxy, useSnapshot } from "valtio";
+import React, { useLayoutEffect } from "react";
 import { Log } from "../Logging";
-import { definePropertiesFromSource } from "../definePropertiesFromSource";
 
 /**
  * An error that occured within a particular operation.
@@ -22,11 +20,16 @@ export interface OperationError {
   error: string;
 }
 
-const modalState = proxy<OperationModalsData>({
+const state: OperationModalsData = ({
   currentOperation: null,
   currentError: null,
   statusText: null,
   logsManuallyOpen: false,
+  setCurrentOperation: undefined,
+  setCurrentError: undefined,
+  setStatusText: undefined,
+  setLogsManuallyOpen: undefined,
+  mounted: false,
 });
 
 interface OperationModalsData {
@@ -39,76 +42,46 @@ interface OperationModalsData {
 
   /** Whether or not the logs have been manually opened by the user. */
   logsManuallyOpen: boolean;
-}
-
-interface OperationModalsActions {
-  /** Creates a function that can be used to set whether or not a particular operation is currently in progress. */
-  useSetWorking: typeof useSetWorking;
-
-  /** Creates a function that can be used to set an error when a particular operation failed. */
-  useSetError: typeof useSetError;
-
-  /** Used to wrap a particular operation while displaying the logging window and any errors if appropriate. */
-  wrapOperation: typeof wrapOperation;
-}
-
-/**
- * Creates a function that can be used to set whether or not a particular operation is currently in progress.
- */
-function useSetWorking(
-  operationName: string
-): (working: boolean) => void {
-  return function setWorking(working) {
-    if (working) {
-      modalState.currentOperation = operationName;
-    } else {
-      modalState.statusText = null;
-      modalState.currentOperation = null;
-    }
-  };
-}
-
-/**
- * Creates a function that can be used to set an error when a particular operation failed.
- */
-function useSetError(
-  errorTitle: string
-): (error: unknown | null) => void {
-  return function setError(error) {
-    if (error === null) {
-      modalState.currentError = null;
-    } else {
-      Log.error(`${errorTitle}: ${String(error)}`);
-      modalState.currentError = {
-        title: errorTitle,
-        error: String(error),
-      };
-    }
-  };
-}
-
-async function wrapOperation(
-  operationName: string,
-  errorModalTitle: string,
-  operation: () => Promise<void>
-) {
-  const setWorking = useSetWorking(operationName);
-  const setError = useSetError(errorModalTitle);
-
-  setWorking(true);
-  try {
-    await operation();
-  } catch (error) {
-    Log.error(errorModalTitle + ": " + error);
-    setError(error);
-  } finally {
-    setWorking(false);
-  }
+  
+  mounted: boolean;
+  
+  setCurrentOperation?: React.Dispatch<React.SetStateAction<string | null>>;
+  setCurrentError?: React.Dispatch<React.SetStateAction<OperationError | null>>;
+  setStatusText?: React.Dispatch<React.SetStateAction<string | null>>;
+  setLogsManuallyOpen?: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 // Component that displays the log window when an operation is in progress, and displays errors when the operation failed.
 export function OperationModals() {
-  const { currentOperation, currentError, statusText, logsManuallyOpen } = useSnapshot(modalState);
+  const [currentOperation, setCurrentOperation] = React.useState<string | null>(null);
+  const [currentError, setCurrentError] = React.useState<OperationError | null>(null);
+  const [statusText, setStatusText] = React.useState<string | null>(null);
+  const [logsManuallyOpen, setLogsManuallyOpen] = React.useState<boolean>(false);
+  
+  state.currentOperation = currentOperation;
+  state.currentError = currentError;
+  state.statusText = statusText;
+  state.logsManuallyOpen = logsManuallyOpen;
+  
+  useLayoutEffect(() => {
+    if (state.mounted) {
+      throw new Error("Multiple OperationModals components mounted. There should only be one.");
+    }
+    
+    state.setCurrentOperation = setCurrentOperation;
+    state.setCurrentError = setCurrentError;
+    state.setStatusText = setStatusText;
+    state.setLogsManuallyOpen = setLogsManuallyOpen;
+    state.mounted = true;
+
+    return () => {
+      state.setCurrentOperation = undefined;
+      state.setCurrentError = undefined;
+      state.setStatusText = undefined;
+      state.setLogsManuallyOpen = undefined;
+      state.mounted = false;
+    };
+  });
 
   const canClose = logsManuallyOpen && currentError === null;
   const needSyncModal =
@@ -121,33 +94,102 @@ export function OperationModals() {
         title={currentOperation ?? "Log output"}
         subtext={statusText}
         onClose={
-          canClose
-            ? () => (modalState.logsManuallyOpen = false)
-            : undefined
+          canClose ? () => (setLogsManuallyOpen(false)) : undefined
         }
       />
       <ErrorModal
         isVisible={currentError !== null}
         title={currentError?.title ?? ""}
         description={currentError?.error}
-        onClose={() => (modalState.currentError = null)}
+        onClose={() => (setCurrentError(null))}
       ></ErrorModal>
     </>
   );
 }
 
-export namespace OperationModals  {
+export namespace OperationModals {
   export let currentOperation: string | null;
   export let currentError: OperationError | null;
   export let statusText: string | null;
   export let logsManuallyOpen: boolean;
-  export let useSetWorking: OperationModalsActions["useSetWorking"];
-  export let useSetError: OperationModalsActions["useSetError"];
-  export let wrapOperation: OperationModalsActions["wrapOperation"];
+
+  /**
+   * Creates a function that can be used to set whether or not a particular operation is currently in progress.
+   */
+  export function useSetWorking(operationName: string): (working: boolean) => void {
+    return function setWorking(working) {
+      if (working) {
+        OperationModals.currentOperation = operationName;
+      } else {
+        OperationModals.statusText = null;
+        OperationModals.currentOperation = null;
+      }
+    };
+  }
+
+  /**
+   * Creates a function that can be used to set an error when a particular operation failed.
+   */
+  export function useSetError(errorTitle: string): (error: unknown | null) => void {
+    return function setError(error) {
+      if (error === null) {
+        OperationModals.currentError = null;
+      } else {
+        Log.error(`${errorTitle}: ${String(error)}`);
+        OperationModals.currentError = {
+          title: errorTitle,
+          error: String(error),
+        };
+      }
+    };
+  }
+
+  export async function wrapOperation(
+    operationName: string,
+    errorModalTitle: string,
+    operation: () => Promise<void>
+  ) {
+    const setWorking = useSetWorking(operationName);
+    const setError = useSetError(errorModalTitle);
+
+    setWorking(true);
+    try {
+      await operation();
+    } catch (error) {
+      Log.error(errorModalTitle + ": " + error);
+      setError(error);
+    } finally {
+      setWorking(false);
+    }
+  }
 }
 
-definePropertiesFromSource(OperationModals, modalState);
-definePropertiesFromSource(OperationModals, {useSetWorking, useSetError, wrapOperation}, undefined, true);
+Object.defineProperties(OperationModals, {
+  currentOperation: {
+    get: () => state.currentOperation,
+    set: (value) => state.setCurrentOperation!(value),
+    enumerable: true,
+    configurable: false,
+  },
+  currentError: {
+    get: () => state.currentError,
+    set: (value) => state.setCurrentError!(value),
+    enumerable: true,
+    configurable: false,
+  },
+  statusText: {
+    get: () => state.statusText,
+    set: (value) => state.setStatusText!(value),
+    enumerable: true,
+    configurable: false, 
+  },
+  logsManuallyOpen: {
+    get: () => state.logsManuallyOpen,
+    set: (value) => state.setLogsManuallyOpen!(value),
+    enumerable: true,
+    configurable: false,
+  }
+})
 
 function SyncingModal({
   isVisible,
